4e8d8174d46b017f7436e0ea661e2ebc838352a8
Tim Boldt <tim.boldt@gmail.com>
2023-05-11 20:11:05 -0700

    Added sidewinder algo

--- cmd/main.go
+++ cmd/main.go
@@ -7,10 +7,13 @@ import (
 )
 
 func main() {
-	m := maze.NewRectangularMaze(15, 30)
-	fmt.Printf("%+v\n", m)
-	m.ApplyBinaryTree()
-	fmt.Printf("%+v\n", m)
-	//m.DrawSVG()
-	m.Print()
+	m1 := maze.NewRectangularMaze(15, 30)
+	m1.ApplyBinaryTree()
+	m1.Print()
+
+	fmt.Printf("\n=========================================================================================================================\n\n")
+
+	m2 := maze.NewRectangularMaze(15, 30)
+	m2.ApplySideWinder()
+	m2.Print()
 }
--- rectangular.go
+++ rectangular.go
@@ -113,3 +113,31 @@ func (m *RectangularMaze) ApplyBinaryTree() {
 		}
 	}
 }
+
+func (m *RectangularMaze) ApplySideWinder() {
+	for r := 0; r < m.rows; r++ {
+		var run []Cell
+		for c := 0; c < m.cols; c++ {
+			run = append(run, Cell{row: r, col: c})
+
+			shouldCloseOut := c+1 == m.cols || (rand.Intn(2) == 0 && r+1 != m.rows)
+
+			if shouldCloseOut {
+				// Pick a random entry from the run.
+				src := run[rand.Intn(len(run))]
+				// Link to the next row from the random cell.
+				m.g.AddEdge(
+					CellHash(src),
+					CellHash(Cell{row: src.row + 1, col: src.col}),
+				)
+				run = nil
+			} else {
+				// Link to next column.
+				m.g.AddEdge(
+					CellHash(Cell{row: r, col: c}),
+					CellHash(Cell{row: r, col: c + 1}),
+				)
+			}
+		}
+	}
+}

ce09a6a987dc707dcf9b3ec4d2a0da11144bbea8
Tim Boldt <tim.boldt@gmail.com>
2023-05-12 22:33:08 -0700

    Added Aldous-Broder algo

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/png"
 	"os"
 
@@ -8,15 +9,26 @@ import (
 )
 
 func main() {
-	m := rectangular.NewMaze(15, 30)
-	m.ApplyBinaryTree()
-	m.FindBestExitPoint()
-	f, _ := os.Create("image_binarytree.png")
-	png.Encode(f, m.ToImage())
+	var m *rectangular.Maze
 
-	m = rectangular.NewMaze(15, 30)
-	m.ApplySideWinder()
+	// fmt.Println("Binary Tree")
+	// m = rectangular.NewMaze(50, 30)
+	// m.ApplyBinaryTree()
+	// m.FindBestExitPoint()
+	// f, _ := os.Create("image_binarytree.png")
+	// png.Encode(f, m.ToImage())
+
+	// fmt.Println("Sidewinder")
+	// m = rectangular.NewMaze(50, 30)
+	// m.ApplySideWinder()
+	// m.FindBestExitPoint()
+	// f, _ = os.Create("image_sidewinder.png")
+	// png.Encode(f, m.ToImage())
+
+	fmt.Println("Aldous-Broder")
+	m = rectangular.NewMaze(50, 30)
+	m.ApplyAldousBroder()
 	m.FindBestExitPoint()
-	f, _ = os.Create("image_sidewinder.png")
+	f, _ := os.Create("image_aldousbroder.png")
 	png.Encode(f, m.ToImage())
 }
---
+++ rectangular/aldousbroder.go
@@ -0,0 +1,63 @@
+package rectangular
+
+import (
+	"math/rand"
+)
+
+func (m *Maze) visited(r, c int) bool {
+	var nr, nc int
+	for direction := 0; direction < 4; direction++ {
+		switch direction {
+		case 0:
+			nr, nc = r+1, c
+		case 1:
+			nr, nc = r-1, c
+		case 2:
+			nr, nc = r, c+1
+		case 3:
+			nr, nc = r, c-1
+		}
+		src := CellHash(Cell{row: r, col: c})
+		dest := CellHash(Cell{row: nr, col: nc})
+		// _, err := m.g.Edge(src, dest)
+		// fmt.Printf("%d - %d: %v\n", src, dest, err)
+		if _, err := m.g.Edge(src, dest); err == nil {
+			return true
+		}
+	}
+	return false
+}
+
+func (m *Maze) ApplyAldousBroder() {
+	m.name = "Aldous-Broder"
+	r := rand.Intn(m.rows)
+	c := rand.Intn(m.cols)
+	unvisited := m.rows*m.cols - 1
+
+	for unvisited > 0 {
+		direction := rand.Intn(4)
+		var nr, nc int
+		switch direction {
+		case 0:
+			nr, nc = r+1, c
+		case 1:
+			nr, nc = r-1, c
+		case 2:
+			nr, nc = r, c+1
+		case 3:
+			nr, nc = r, c-1
+		}
+		if nr >= 0 && nr < m.rows && nc >= 0 && nc < m.cols {
+			//fmt.Printf("(%d,%d): %v\n", nr, nc, m.visited(nr, nc))
+			if !m.visited(nr, nc) {
+				src := CellHash(Cell{row: r, col: c})
+				dest := CellHash(Cell{row: nr, col: nc})
+				m.g.AddEdge(src, dest)
+				unvisited--
+			}
+
+			r = nr
+			c = nc
+		}
+	}
+}

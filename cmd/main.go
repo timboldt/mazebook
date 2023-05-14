9dfca70a8bcf658a469b0754a63115078ccbf619
Tim Boldt <tim.boldt@gmail.com>
2023-05-13 21:57:10 -0700

    Minor cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -10,28 +10,29 @@ import (
 
 func main() {
 	var m *rectangular.Maze
+	var f *os.File
 
-	// fmt.Println("Binary Tree")
-	// m = rectangular.NewMaze(50, 30)
-	// m.ApplyBinaryTree()
-	// m.FindBestExitPoint()
-	// m.Print()
-	// f, _ := os.Create("image_binarytree.png")
-	// png.Encode(f, m.ToImage())
+	fmt.Println("Binary Tree")
+	m = rectangular.NewMaze(50, 30)
+	m.ApplyBinaryTree()
+	m.FindBestExitPoint()
+	m.Print()
+	f, _ = os.Create("image_binarytree.png")
+	png.Encode(f, m.ToImage())
 
-	// fmt.Println("Sidewinder")
-	// m = rectangular.NewMaze(50, 30)
-	// m.ApplySideWinder()
-	// m.FindBestExitPoint()
-	// m.Print()
-	// f, _ := os.Create("image_sidewinder.png")
-	// png.Encode(f, m.ToImage())
+	fmt.Println("Sidewinder")
+	m = rectangular.NewMaze(50, 30)
+	m.ApplySideWinder()
+	m.FindBestExitPoint()
+	m.Print()
+	f, _ = os.Create("image_sidewinder.png")
+	png.Encode(f, m.ToImage())
 
 	fmt.Println("Aldous-Broder")
 	m = rectangular.NewMaze(50, 30)
 	m.ApplyAldousBroder()
 	m.FindBestExitPoint()
 	m.Print()
-	f, _ := os.Create("image_aldousbroder.png")
+	f, _ = os.Create("image_aldousbroder.png")
 	png.Encode(f, m.ToImage())
 }
